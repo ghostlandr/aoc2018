c698943211663ffe2d58448a701470c370fe9b25
Graham Holtslander <gholtslander@vendasta.com>
2018-12-06 06:43:02 -0600

    Add day 6 input and starter files

---
+++ d6/part2.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	data, _ := ioutil.ReadFile("/Users/gholtslander/go/src/github.com/gholtslander-va/aoc2018/d6/input.txt")
+	stringData := strings.Split(string(data), "\n")
+
+	for _, s := range stringData {
+		fmt.Println(s)
+	}
+}
